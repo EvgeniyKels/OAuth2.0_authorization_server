0d0fb1aad30639af08cfe3bdcd24723b363352a7
evgeniykels <evgeniikels@gmail.com>
2020-06-13 14:50:25 +0300

    _

--- src/main/java/kls/oauth/authserver/config/SecurityConfiguration.java
+++ src/main/java/kls/oauth/authserver/config/SecurityConfiguration.java
@@ -35,7 +35,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated().and().sessionManagement()
+        http.authorizeRequests().
+                anyRequest().authenticated().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.NEVER);
     }
 
